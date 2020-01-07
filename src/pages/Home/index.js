be7df2243ca03790c28dd5bff1c7cc8ec4f24fc9
Bruno de Oliveira <bruno.mktd@gmail.com>
2020-01-06 23:54:04 -0300

    Initial commit from Create React App and Redux

---
+++ src/pages/Home/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { ProductList } from './styles';
+
+class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  handleAddProduct = product => {
+    const { addToCart } = this.props;
+
+    addToCart(product);
+  };
+
+  render() {
+    const { products } = this.state;
+    const { amount } = this.props;
+
+    return (
+      <ProductList>
+        {products.map(p => (
+          <li key={p.id}>
+            <img src={p.image} alt={p.title} />
+            <strong>{p.title}</strong>
+            <span>{p.priceFormatted}</span>
+            <button type="button" onClick={() => this.handleAddProduct(p)}>
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {''} {amount[p.id] || 0}
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, p) => {
+    amount[p.id] = p.amount;
+
+    return amount;
+  }, {}),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
